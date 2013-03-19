7a1039906d15a6484bd3a66f0c305661c3c9f638
Jesse La Russo <jesse.larusso@flatironschool.com>
2013-03-19 15:05:38 -0400

    updated codeschool model

--- app/models/codeschool.rb
+++ app/models/codeschool.rb
@@ -1,15 +1,20 @@
 require 'open-uri'
 
 class CodeSchool < ActiveRecord::Base
+  attr_accessor :cs_json
 
-  def get_json
-    doc = open('http://www.codeschool.com/users/dolinsky.json').read
-    JSON.parse(doc)
+  def initialize(url) # url = 'http://www.codeschool.com/users/dolinsky.json'
+    get_json(url)
   end
 
-  def self.get_data_from_codeschool
-    username = cs_hash["user"]["username"]
-    completed_coures = cs_hash["courses"]["completed"].collect do |c|
+  def get_json(url)
+    doc = open(url).read
+    @cs_json = JSON.parse(doc)
+  end
+
+  def get_data_from_codeschool
+    username = @cs_json["user"]["username"]
+    completed_coures = @cs_json["courses"]["completed"].collect do |c|
       c["title"]
     end
   end
