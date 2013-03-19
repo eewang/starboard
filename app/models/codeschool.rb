92fdcb7b84b9d026472c95c04ff74f61b610459c
Danny Olinsky <dannyolinsky@gmail.com>
2013-03-19 15:50:40 -0400

    Create CodeSchool Class

--- app/models/codeschool.rb
+++ app/models/codeschool.rb
@@ -1,25 +1,30 @@
 require 'open-uri'
 
 class CodeSchool < Achievement
-  attr_accessor :cs_json
 
-  def initialize(url) # url = 'http://www.codeschool.com/users/dolinsky.json'
-    get_json(url)
-  end
+  # def initialize(url) # url = 'http://www.codeschool.com/users/dolinsky.json'
+  #   get_json(url)
+  # end
 
-  def get_json(url)
+  def get_json('http://www.codeschool.com/users/dolinsky.json')
     doc = open(url).read
-    @cs_json = JSON.parse(doc)
+    cs_json = JSON.parse(doc)
+    get_data_from_codeschool(cs_json)
   end
 
-  def get_data_from_codeschool
-    completed_coures = @cs_json["courses"]["completed"].collect do |c|
+  def get_data_from_codeschool(cs_json)
+    completed_coures = cs_json["courses"]["completed"].collect do |c|
       c["title"]
     end
+    # raise completed_courses.inspect
+    create_achievement(completed_courses)
   end
 
-  def create_achievement
-    user.achievements.build(somekindofparams)
+  def create_achievement(completed_courses)
+    user = User.create(:name => "Danny")
+    completed_courses.each do |c|
+      user.achievements.create(:name => c)
+    end
   end
 
 end
