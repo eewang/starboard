d6acd74ab3e64c5759474e791e7e206fb5929c9c
Danny Olinsky <dannyolinsky@gmail.com>
2013-03-19 14:36:39 -0400

    Codeschool basic parse

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,7 +1,22 @@
+require 'open-uri'
+
 class Achievement < ActiveRecord::Base
   attr_accessible :star_id, :user_id
 
   belongs_to :user
   belongs_to :star
-  
+
+  def self.get_from_codeschool
+    doc = open('http://www.codeschool.com/users/dolinsky.json').read
+    cs_hash = JSON.parse(doc)
+
+    name = cs_hash["user"]["username"]
+    completed_coures = cs_hash["courses"]["completed"].each do |c|
+      puts c["title"]
+    end
+    nil
+  end
+
+
+
 end
