71351deb6528aa83224a102a009dd5555cadc43c
Danny Olinsky <dannyolinsky@gmail.com>
2013-03-20 17:28:56 -0400

    Getting closer to creating stars from blog posts

---
+++ app/models/blog.rb
@@ -0,0 +1,44 @@
+require 'pry'
+require 'pp'
+require 'feedzirra'
+
+class Blog
+
+  attr_accessor :feed, :num_entries
+
+  def blog_url
+    user.blog_link #write a method in user for blog link
+    # url = "http://1aurabrown.github.com/atom.xml"
+    @blog_url
+  end
+
+  def get_feed
+    @feed = Feedzirra::Feed.fetch_and_parse("http://dolin.github.com/atom.xml")
+  end
+
+  def get_entries_titles
+    @feed.entries.collect do |entry|
+      entry.title
+    end
+  end
+
+  def get_created_at_times
+    @feed.entries.collect do |entry|
+      entry.published
+    end
+  end
+
+  def count_entries
+    @num_entries = @feed.entries.count
+  end
+
+  def create_blog_achievements
+    user = User.create(:name => "Victoria")
+    #compare num_titles to number of current stars for blog posts?
+    @num_entries.each do |entry|
+      star = Star.where(:name => "Blog Post").first #create stars for each completed course
+      user.achievements.create(:star_id => star.id)
+    end
+  end
+
+end
--- doc/blogxmltest.rb
+++ doc/blogxmltest.rb
@@ -1,18 +1,16 @@
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
-require 'pp'
+# require 'nokogiri'
+# # require 'open-uri'
+# # require 'pry'
+# require 'pp'
 
-Class Blogpost
+#   url = "http://1aurabrown.github.com/atom.xml"
+#   doc = Nokogiri::XML(open(url))
 
-  url = "http://1aurabrown.github.com/atom.xml"
-  doc = Nokogiri::XML(open(url))
+#   titles = doc.xpath("//xmlns:title").collect do |title|
+#     title.text
+#   end
 
-  titles = doc.xpath("//xmlns:title").collect do |title|
-    title.text
-  end
-
-  titles = titles.shift
+#   titles = titles.shift
 
 
 
