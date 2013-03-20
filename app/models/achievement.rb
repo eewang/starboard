9a999c4cdea94b1e7cf4d6012b1059fbee635585
Victoria Friedman <victoria.m.friedman@gmail.com>
2013-03-20 17:27:22 -0400

    create achievement from user

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,10 +1,11 @@
 require 'open-uri'
 
 class Achievement < ActiveRecord::Base
-  attr_accessible :star_id, :user_id
+  attr_accessible :star_id, :user_id, :sender, :receiver, :message, :star
 
   belongs_to :user
   belongs_to :star
+
 
   def self.get_from_codeschool
     doc = open('http://www.codeschool.com/users/dolinsky.json').read
@@ -14,4 +15,16 @@ class Achievement < ActiveRecord::Base
     end
   end
 
+
+  def self.create_from_user(sender_id, receiver_id, message)
+    @sender = User.find(sender_id)
+    @receiver = User.find(receiver_id)
+    star = Star.where(:name => "teacher star").first_or_create
+    @star_id = star.id
+  end
+
+end
+
+def create_star_from_user
+  Achievement.create_from_user(params[:sender], params[:receiver], params[:message])
 end
