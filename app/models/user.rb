deeb9317df0062cd3b66e427d9cb433edec24321
Danny Olinsky <dannyolinsky@gmail.com>
2013-03-21 15:55:18 -0400

    Ability to create a new user from user#new and save codeschool achievements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,25 @@
 class User < ActiveRecord::Base
+  before_save :get_user_data
+
   attr_accessible :name, :profile_pic, :treehouse_username, :codeschool_username, :blog_url
 
   has_many :achievements
   has_many :stars, :through => :achievements
 
+
+  def get_user_data
+    codeschool = Codeschool.get_codeschool_data(self.codeschool_username)
+    check_achievements_by_array(codeschool)
+  end
+
+  def check_achievement_by_string(string)
+    star = Star.where(:name => string).first_or_create
+    self.achievements.build(:star_id => star.id)
+  end
+
+  def check_achievements_by_array(array)
+    array.each { |item| check_achievement_by_string(item) }
+  end
 
 end
 
