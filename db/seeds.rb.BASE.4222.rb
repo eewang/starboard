acb3a2619ea8057b1ab78215d7e613903319564a
Jesse La Russo <jesse.larusso@flatironschool.com>
2013-03-21 10:47:27 -0400

    seed

---
+++ db/seeds.rb.BASE.4222.rb
@@ -0,0 +1,37 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([ { name: 'Jesse'},
+                      { name: 'Victoria'},
+                      { name: 'Bob'},
+                      { name: 'Tyler'},
+                      { name: 'Danny'},
+                      { name: 'Jane'},
+                      { name: 'Masha'},
+                      { name: 'Harrison'},
+                      { name: 'Eric'},
+                      { name: 'Sam'},
+                      { name: 'Person'},
+                      { name: 'Chuck'},
+                      { name: 'Sarah'},
+                      { name: 'John'},
+                      { name: 'Mark'},
+                      { name: 'Paul'},
+                      { name: 'Vinnie'},
+                      { name: 'Tina'},
+                      { name: 'Trixie'},
+                      { name: 'Daniel'},
+                      { name: 'Tim'},
+                      { name: 'Christina'},
+                      { name: 'Erin'},
+                      { name: 'Ei-lene'},
+                      ])
+
+User.all.each do |user|
+  user.profile_pic = "http://lorempixel.com/210/210/people/"
+end
