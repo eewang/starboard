b4e9d98a726baaa8f4c0ab96820b868f52f2942f
Jesse La Russo <jesse.larusso@flatironschool.com>
2013-03-21 11:10:03 -0400

    create seed data for users and profile_pic

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,8 +32,7 @@ users = [{ name: 'Jesse'},
         { name: 'Ei-lene'},
         ]
 
-users.each do |user|
-  u = User.find_or_create_by_name(user[:name])
-  u.profile_pic = "http://lorempixel.com/210/210/people/"
-  u.save
+User.all.each do |user|
+  user.profile_pic = "http://lorempixel.com/210/210/people/"
+  user.save
 end
