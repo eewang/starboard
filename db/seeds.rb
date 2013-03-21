fea5a14c13211d0465021f6144e56a576684e01e
Jesse La Russo <jesse.larusso@flatironschool.com>
2013-03-21 10:47:27 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([ { name: 'Jesse'},
+                      { name: 'Victoria'},
+                      { name: 'Bob'},
+                      { name: 'Tyler'},
+                      { name: 'Danny'},
+                      { name: 'Jane'},
+                      { name: 'Masha'},
+                      { name: 'Harrison'},
+                      { name: 'Eric'},
+                      { name: 'Sam'},
+                      { name: 'Person'},
+                      { name: 'Chuck'},
+                      { name: 'Sarah'},
+                      { name: 'John'},
+                      { name: 'Mark'},
+                      { name: 'Paul'},
+                      { name: 'Vinnie'},
+                      { name: 'Tina'},
+                      { name: 'Trixie'},
+                      { name: 'Daniel'},
+                      { name: 'Tim'},
+                      { name: 'Christina'},
+                      { name: 'Erin'},
+                      { name: 'Ei-lene'},
+                      ])
+
+User.all.each do |user|
+  user.profile_pic = "http://lorempixel.com/210/210/people/"
+end
