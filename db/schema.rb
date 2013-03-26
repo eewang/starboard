83076e972571226fdec359dd09b24e2ef1df6629
Danny Olinsky <dannyolinsky@gmail.com>
2013-03-26 13:33:45 -0400

    Created source table to hold sources for all stars

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130326142216) do
+ActiveRecord::Schema.define(:version => 20130326173213) do
 
   create_table "achievements", :force => true do |t|
     t.integer  "user_id"
@@ -30,11 +30,18 @@ ActiveRecord::Schema.define(:version => 20130326142216) do
     t.datetime "updated_at",  :null => false
   end
 
+  create_table "sources", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "stars", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "type"
+    t.integer  "source_id"
   end
 
   create_table "users", :force => true do |t|
