d4b9dea9fcf09118f6a11f1e82f73af6d1873679
Jesse La Russo <jesse.larusso@flatironschool.com>
2013-03-26 16:05:31 -0400

    stars on the user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,7 +66,7 @@ class User < ActiveRecord::Base
 
   def get_profile_pic_from_email(email)
     hash = Digest::MD5.hexdigest(email.strip.downcase)
-    "http://www.gravatar.com/avatar/" + hash + "?s=140"
+    "http://www.gravatar.com/avatar/" + hash + "?s=160"
   end
 
   def get_profile_pic
