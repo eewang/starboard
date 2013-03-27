da62e2c8d4d3fe6174359a63e46ed792c5e09812
Danny Olinsky <dannyolinsky@gmail.com>
2013-03-27 11:41:18 -0400

    Updated seed data with more user info. Unneccesary data has been removed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -71,9 +71,99 @@ users = User.first_or_create([
     stackoverflow_username: 'john-kelly-ferguson',
     password: 'hella',
     password_confirmation: 'hella'
+  }, 
+  {
+    name: 'Jane Vora',
+    email: 'jane.vora@flatironschool.com',
+    blog_url: 'http://janeeats.github.com/atom.xml',
+    github_username: 'janeeats',
+    codeschool_username: 'janeeats',
+    treehouse_username: 'janeeats',
+    stackoverflow_username: 'janeeatsless',
+    password: 'hella',
+    password_confirmation: 'hella'
+  },
+  {
+    name: 'Eric Iacutone',
+    email: 'eric.iacutone@flatironschool.com',
+    blog_url: 'http://iacutone.github.com/atom.xml',
+    github_username: 'iacutone',
+    codeschool_username: 'iactuone',
+    treehouse_username: 'iactuone',
+    stackoverflow_username: 'iactuone',
+    password: 'hella',
+    password_confirmation: 'hella'
+  },
+  {
+    name: 'Jesse Larusso',
+    email: 'jesse.larusso@flatironschool.com',
+    blog_url: 'http://jlarusso.github.com/atom.xml',
+    github_username: 'jlarusso',
+    codeschool_username: 'jlarusso',
+    treehouse_username: 'jesselarusso',
+    stackoverflow_username: 'jlarusso',
+    password: 'hella',
+    password_confirmation: 'hella'
+  },
+  {
+    name: 'Ei-lene Heng',
+    email: 'eilene.heng@flatironschool.com',
+    blog_url: 'http://ei-lene.github.com/atom.xml',
+    github_username: 'eilene',
+    codeschool_username: 'eilene',
+    treehouse_username: 'eilene',
+    stackoverflow_username: 'ei-lene',
+    password: 'hella',
+    password_confirmation: 'hella'
+  },
+  {
+    name: 'Christina Chang',
+    email: 'christina.chang@flatironschool.com',
+    blog_url: 'http://christinachang.github.com/atom.xml',
+    github_username: 'christinachang',
+    codeschool_username: 'christinachang',
+    treehouse_username: 'christinachang',
+    stackoverflow_username: 'christinachang',
+    password: 'hella',
+    password_confirmation: 'hella'
+  },
+  {
+    name: 'Rahul Seshan',
+    email: 'rahul.seshan@flatironschool.com',
+    blog_url: 'http://rseshan.github.com/atom.xml',
+    github_username: 'rseshan',
+    codeschool_username: 'rseshan',
+    treehouse_username: 'rseshan',
+    stackoverflow_username: 'rseshan',
+    password: 'hella',
+    password_confirmation: 'hella'
+  },
+  {
+    name: 'Cho Kim',
+    email: 'cho.kim@flatironschool.com',
+    blog_url: 'http://chokim.github.com/atom.xml',
+    github_username: 'ChoKim',
+    codeschool_username: 'ChoKim',
+    treehouse_username: 'chokim',
+    stackoverflow_username: 'ChoKim',
+    password: 'hella',
+    password_confirmation: 'hella'
+  },
+  {
+    name: 'Andrew Callahan',
+    email: 'andrew.callahan@flatironschool.com',
+    blog_url: 'http://andrewcallahan.tumblr.com/rss',
+    github_username: 'andrewcallahan',
+    codeschool_username: 'andrewcallahan',
+    treehouse_username: 'andrewcallahan',
+    stackoverflow_username: 'andrewcallahan',
+    password: 'hella',
+    password_confirmation: 'hella'
   }
 ])
 
 stars = Star.first_or_create([
   { name: 'Gifted Star'}
 ])
+
+
