aaeb46c692d84d10c4c282b59b052346a5153156
Tyler Davis <tyebud@gmail.com>
2013-03-27 16:27:50 -0400

    Hella catz

--- app/assets/javascripts/angular/controllers/grid_list_controllers.js
+++ app/assets/javascripts/angular/controllers/grid_list_controllers.js
@@ -2,6 +2,24 @@ function GridListController($scope, $http) {
   $http.get('/users.json').success(function (data) {
     $scope.users = data;
     $scope.orderProp = '-achievements';
-  });
+    $scope.grid = 320;
+    $scope.gridEls = $('.group-grid .user');
 
+    $scope.zoomIn = function ($event) {
+      if ($scope.grid < 440) {
+        $scope.grid = $scope.grid * 1.1
+        console.log($scope.grid);
+        $('.group-grid .user').animate({ 'height' : String($scope.grid) + 'px',
+                        'width'  : String($scope.grid) + 'px' }, 100, 'swing');
+      }
+    }
+    
+    $scope.zoomOut = function ($event) {
+      if ($scope.grid > 137) {
+        $scope.grid = $scope.grid * 0.9
+        $('.group-grid .user').animate({ 'height' : String($scope.grid) + 'px',
+                        'width'  : String($scope.grid) + 'px' }, 100, 'swing');
+      };
+    }
+  })
 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -71,7 +71,7 @@ class User < ActiveRecord::Base
 
   def get_profile_pic_from_email(email)
     hash = Digest::MD5.hexdigest(email.strip.downcase)
-    "http://www.gravatar.com/avatar/" + hash + "?s=320"
+    "http://www.gravatar.com/avatar/" + hash + "?s=440"
   end
 
   def get_profile_pic
