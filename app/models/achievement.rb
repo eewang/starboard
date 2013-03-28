c52ddc5b1eb7d9e6e8e11121e0ae5f80681aab05
Jesse La Russo <jesse.larusso@flatironschool.com>
2013-03-28 18:43:43 -0400

    time stuff

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -18,6 +18,18 @@ class Achievement < ActiveRecord::Base
     end
   end
 
+  def distance_of_time_in_hours_and_minutes(from_time, to_time)
+    from_time = from_time.to_time if from_time.respond_to?(:to_time)
+    to_time = to_time.to_time if to_time.respond_to?(:to_time)
+    distance_in_hours   = (((to_time - from_time).abs) / 3600).round
+    distance_in_minutes = ((((to_time - from_time).abs) % 3600) / 60).round
+
+    difference_in_words = ''
+
+    difference_in_words << "#{distance_in_hours} #{distance_in_hours > 1 ? 'hours' : 'hour' } and " if distance_in_hours > 0
+    difference_in_words << "#{distance_in_minutes} #{distance_in_minutes == 1 ? 'minute' : 'minutes' }"
+  end
+
   def as_json(option={})
     {
       :id               => self.id,
