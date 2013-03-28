bd9e2706f1933766c28b7345e22055fd50192cf1
Danny Olinsky <dannyolinsky@gmail.com>
2013-03-28 16:33:55 -0400

    Created view for handraise stars

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
     @gifted_stars = @user.achievements.where(:star_id => gifted_star_id)
     @teacher_stars = Star.where(:source_id => 5)
     @stars_from_teacher = @user.stars.where(:source_id => 5)
+    @handraise_stars = @user.stars.where(:source_id => 6)
 
     respond_to do |format|
       format.html # show.html.erb
