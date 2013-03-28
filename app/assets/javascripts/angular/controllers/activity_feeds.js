9a9298ed2cb0275ce3cae0ee90d5f6f49c0d364e
Tyler Davis <tyebud@gmail.com>
2013-03-28 18:20:01 -0400

    Limited activity feed to 20 items and added timed requests

--- app/assets/javascripts/angular/controllers/activity_feeds.js
+++ app/assets/javascripts/angular/controllers/activity_feeds.js
@@ -1,21 +1,30 @@
+function updateMax (data) {
+  for(var i=0;i<data.length;i++) {
+   if(parseInt(data[i].id,10) > max) {
+      max = data[i].id;
+      maxIndex = i;
+    }
+  }
+}
+
 function ActivityFeedsController ($scope, $http) {
   $http.get('/achievements/newest/0.json').success(function (data) {
     $scope.achievements = data;
     $scope.orderProp = '-id';
-    
+
     var max = 0, maxIndex = -1;
-    for(var i=0;i<data.length;i++) {
-       if(parseInt(data[i].id,10) > max) {
-          max = data[i].id;
-          maxIndex = i;
-       }
-    }
+
+    updateMax($scope.achievements);
 
     var interval = setInterval(function () {
       $http.get('/achievements/newest/' + max + '.json').success(function (data) {
-        $('.achievements')
+        // Prepend the scope achievements with the new data,
+        // make sure the result is only 20 items long
+        $scope.achievements = data.concat($scope.achievements).splice(0, 19);
+        updateMax($scope.achievements);
+        $scope.$$phase || $scope.$apply();
       });
-    }, 5000);
+    }, 1000);
 
   });
 }
