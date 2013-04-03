8b79cb1fa317e445c24cd97899bb932e7d4cffe8
Victoria Friedman <victoria.m.friedman@gmail.com>
2013-04-03 16:42:27 -0400

    drop password column from groups table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130403203325) do
+ActiveRecord::Schema.define(:version => 20130403204136) do
 
   create_table "achievements", :force => true do |t|
     t.integer  "user_id"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(:version => 20130403203325) do
     t.integer  "creator_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "password"
   end
 
   create_table "invitations", :force => true do |t|
