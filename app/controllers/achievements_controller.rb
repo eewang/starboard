2bb2ffb76c74cc36b3674f5a121a298402988da1
Danny Olinsky <dannyolinsky@gmail.com>
2013-04-04 17:49:08 -0400

    modal now builds achievements. need to make render and flash notice

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,10 +1,15 @@
 class AchievementsController < ApplicationController
 
   def create
+
     @achievement = Achievement.new(params[:achievement])
     star_id = Star.where(:name => "Gifted Star").first.id
-    sender_id = current_user.id
-    achievement.create()
+    @achievement.sender_id = current_user.id
+    @achievement.star_id = star_id
+    user_id = User.where(:name => params[:user_name]).first.id
+    @achievement.user_id = user_id
+
+    redirect_to 'groups#show'
   end
 
 end
