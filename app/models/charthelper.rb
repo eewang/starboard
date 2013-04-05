009b91631202138eec43dd3101a58db34b118366
Jesse La Russo <jesse.larusso@flatironschool.com>
2013-04-05 12:28:34 -0400

    x axis now banged out

--- app/models/charthelper.rb
+++ app/models/charthelper.rb
@@ -1,11 +1,18 @@
 class ChartHelper
 
-  def build_x_axis
-    x_categories = Array.new(15)
-    x_categories = x_categories.each_with_index.collect do |day, index|
+  # to get an array of timestamps (2013-03-22 12:05:24 -0400)
+  def self.build_x_axis
+    days = Array.new(15).each_with_index.collect do |day, index|
       day = (Time.now  - index.day)
     end
-    x_categories.reverse.join(', ')
+    format_x_axis(days.reverse)
   end
 
+  # to get an array of formatted days ("3/23")
+  def self.format_x_axis(timestamp_array)
+    timestamp_array.collect! do |timestamp|
+      "#{timestamp.month}/#{timestamp.day}"
+    end
+    timestamp_array.join(', ')
+  end
 end
