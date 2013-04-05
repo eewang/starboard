377cf62f0361666d6404411dc48f3d88646e1302
Jesse La Russo <jesse.larusso@flatironschool.com>
2013-04-05 12:39:07 -0400

    charthelper now polymorphic (as_string) or (as_timestamps)

--- app/models/charthelper.rb
+++ app/models/charthelper.rb
@@ -1,11 +1,19 @@
 class ChartHelper
 
   # to get an array of timestamps (2013-03-22 12:05:24 -0400)
-  def self.build_x_axis
+  def self.build_x_axis(options = {})
     days = Array.new(15).each_with_index.collect do |day, index|
       day = (Time.now  - index.day)
     end
-    format_x_axis(days.reverse)
+    days = days.reverse
+
+    if options[:as_string]
+      format_x_axis(days)
+    elsif options[:as_timestamps]
+      days
+    else 
+      raise(ArgumentError, "Argument must be (:as_string => true) or (:as_timestamps => true)")
+    end
   end
 
   # to get an array of formatted days ("3/23")
