dc0cb83982d9f7652e984407e993235d6ca57cc2
Jesse La Russo <jesse.larusso@flatironschool.com>
2013-04-05 13:38:04 -0400

    ready to test stuff

--- app/models/charthelper.rb
+++ app/models/charthelper.rb
@@ -1,6 +1,13 @@
 class ChartHelper
+  # With this helper, you can call:
+    # ChartHelper.build_x_axis(:as_string => true)
+    # ChartHelper.build_x_axis(:as_timetamp => true)
+    # ChartHelper.build_achievement_data(user_object)
 
-  # to get an array of timestamps (2013-03-22 12:05:24 -0400)
+  # Get an array of:
+    # timestamps (2013-03-22 12:05:24 -0400)
+      # -or-
+    # formatted days ("3/23")
   def self.build_x_axis(options = {})
     days = Array.new(15).each_with_index.collect do |day, index|
       day = (Time.now  - index.day)
@@ -16,11 +23,44 @@ class ChartHelper
     end
   end
 
-  # to get an array of formatted days ("3/23")
   def self.format_x_axis(timestamp_array)
     timestamp_array.collect! do |timestamp|
       "#{timestamp.month}/#{timestamp.day}"
     end
     timestamp_array.join(', ')
+  end
+
+
+  # Count achievements for a user by day
+  def self.build_achievement_data(user)
+    day_array = user.achievements.includes.collect do |achievement|
+      "#{achievement.created_at.month}/#{achievement.created_at.day}"
+    end
+
+    count_hash = count_achievements_by_day(day_array)
+    timestamp_array = ChartHelper.build_x_axis(:as_timestamps => true)
+
+    compare_days(count_hash, timestamp_array)
+  end
+
+  def self.count_achievements_by_day(day_array)
+    hash = Hash.new(0)
+    day_array.each do |day|
+      hash[v] += 1
+    end
+  end
+
+  def self.compare_days(count_hash, timestamp_array)
+    compare_array = timestamp_array.collect do |timestamp|
+      "#{timestamp.month}/#{timestamp.day}"
+    end
+
+    final_answer = []
+    count_hash.each do |key, value|
+      if compare_array.include? key
+        final_answer << value
+      end
+    end
+    final_answer
   end
 end
