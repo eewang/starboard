c9eafc56e32a131b5b154735ccce63e58b9bba6f
Danny Olinsky <dannyolinsky@gmail.com>
2013-04-08 17:13:15 -0400

    took out spinner

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -34,42 +34,5 @@ $(document).ready(function(){
 
 })
 
-// loading button
-
-function hideLoading() {
-    // other code can be placed here 
-    $("#loading").hide();
-    // other code can be placed here
-}
-
-function showLoading() {
-    // other code can be placed here 
-    $("#loading").show();
-    // other code can be placed here
-}
-
-function dosomething() {
-    showLoading();
-    // simulate a task that can take 3 seconds
-    // afterwards show hide the loading GIF
-    setTimeout("hideLoading()", 5000);
-    return false;
-}
-
-// $(function(){
-//     $('#update-button').click(dosomething());
-// });
-
-$(function(){
-    $("#update-user").on('click', (function(event){ 
-          event.preventDefault();
-          // not sure if below is correct
-          $(this).submit;
-          dosomething();
-    }));
-});
-
-
-
 
 
