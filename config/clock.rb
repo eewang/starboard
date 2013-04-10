8191468e1eee66f0ff50e4487b91e4cd72264f72
Tyler Davis <tyebud@gmail.com>
2013-04-10 12:45:55 -0400

    Get rid of whenever.  Use clockwork

---
+++ config/clock.rb
@@ -0,0 +1,15 @@
+require 'clockwork'
+include Clockwork
+
+Clockwork.configure do |config|
+  config[:sleep_timeout] = 5
+  config[:logger] = Logger.new('./log/clockwork.log')
+end
+
+handler do |job|
+  puts "Running #{job}"
+end
+
+every 5.minutes, 'update_users' do
+  User.update_all
+end
