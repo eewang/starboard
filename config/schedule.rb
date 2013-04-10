a90810ec6b11e9c276ff0e8c07f84938008699a0
Tyler Davis <tyebud@gmail.com>
2013-04-10 12:50:48 -0400

    a

--- config/schedule.rb
+++
@@ -1,9 +0,0 @@
-env :PATH, ENV["PATH"]
-env "GEM_HOME", ENV["GEM_HOME"]
-
-set :output, "./log/whenever.log"
-set :job_template, "/bin/bash -i -c ':job'"
-
-every 5.minutes do
-  runner "User.update_all_users"
-end
