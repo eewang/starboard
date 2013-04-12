6dfc9f4794a80e2c452c9ed650549a1bc59a861a
Jesse La Russo <jesse.larusso@flatironschool.com>
2013-04-12 16:07:49 -0400

    removed some unnecessary comments in handraise controller

--- app/controllers/handraise_controller.rb
+++ app/controllers/handraise_controller.rb
@@ -1,19 +1,9 @@
 class HandraiseController < ApplicationController
 
   def create
-    puts "----------------------------------"
-    puts params
-    puts "----------------------------------"
-
-
     if params[:token] == "3575dbf50eb70f3cdd15"
-
       source_id = Source.where(:name => "Handraise").first.id
-
       star = Star.where(:source_id => source_id).find_or_create_by_name("Handraise")
-
-      # reason = params[:reason]
-
       user = User.where(:email => params[:goldstar_email]).first
 
       user.achievements.create(:star_id => star.id)   
