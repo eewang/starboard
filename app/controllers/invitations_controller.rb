0bcce3d2380a600a30c2dd8ffb1e760446630cc7
Danny Olinsky <dannyolinsky@gmail.com>
2013-04-14 16:56:51 -0400

    can now add individual students to group after group creation

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,16 +2,21 @@ class InvitationsController < ApplicationController
 
   def new
     @invitation = Invitation.new
+    @teacher_groups = current_user.groups.where(:creator_id => current_user.id)
   end
 
   def create
     @invitation = Invitation.new
 
     if @invitation.save
-      InvitationMailer.deliver_invitation(@invitation)
-      format.html redirect_to @invitation, notice: 'Your group invitations have been sent'
+        if params[:emails]
+          EmailsWorker.perform_async(params[:id], params[:emails])
+        end
+        flash[:success] = "emails are on their way out!"
+        redirect_to current_user
     else
-      format.html { render action: "new" }
+        flash[:alert] = "Sorry, group didn't save..."
+        redirect_to new_group_path
     end
   end
 
