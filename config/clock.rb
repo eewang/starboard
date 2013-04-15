53eb8f6978307b96ea6f62ede2b6a53d6b15852b
Tyler Davis <tyebud@gmail.com>
2013-04-15 12:34:01 -0400

    Re-added clock file

---
+++ config/clock.rb
@@ -0,0 +1,17 @@
+require 'clockwork'
+require 'config/boot'
+require 'config/environment'
+include Clockwork
+
+Clockwork.configure do |config|
+  config[:sleep_timeout] = 5
+  config[:logger] = Logger.new('./log/clockwork.log')
+end
+
+handler do |job|
+  puts "Running #{job}"
+end
+
+every 5.minutes, 'update_users' do
+  User.update_all
+end
