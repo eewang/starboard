3bd8052868976186f6f4854771e4b78f6223d036
Tyler Davis <tyebud@gmail.com>
2013-04-15 13:06:57 -0400

    Deploy.rb tweak

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,8 +44,6 @@ namespace :customs do
     run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/user_pass.yml #{release_path}/config/user_pass.yml"
     run "ln -nfs #{shared_path}/sidekiq.yml #{release_path}/config/sidekiq.yml"
-    run "bundle exec RAILS_ENV=production clockworkd --clock=lib/clock.rb stop"
-    run "bundle exec RAILS_ENV=production clockworkd --clock=lib/clock.rb start"
   end
 end
 
