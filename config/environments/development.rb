0f64fe79b4c5090b4893f85bf54572b34faeb7d7
Danny Olinsky <dannyolinsky@gmail.com>
2013-04-16 16:27:27 -0400

    fixed js loading issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,7 @@ Starboard::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
+  config.serve_static_assets = false
 end
 
 #developmentrb, user_pass, application.rb
