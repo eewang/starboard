ed6cd916ede94afe04625f71d06cedd33e011e5b
Jesse La Russo <jesse.larusso@flatironschool.com>
2013-04-16 09:58:33 -0400

    update badges returns badge data as an array

--- app/models/treehouse_chart.rb
+++ app/models/treehouse_chart.rb
@@ -6,7 +6,8 @@ class TreehouseChart
   def self.update_badges(user)
     badges_array = TreehouseChart.get_data(user.treehouse_username)
     x_axis_days_array = TreehouseChart.build_x_axis
-    badges_array
+    
+    count_badges_by_day(x_axis_days_array, badges_array)
   end
 
   def self.get_data(username)
@@ -16,8 +17,9 @@ class TreehouseChart
 
   def self.parse_badges(json)
     earned_dates = json["badges"].collect { |badge| badge["earned_date"] }
-    earned_dates.collect do |date|
-      Time.parse(date)
+    earned_dates.collect do |day|
+      date = Time.parse(day)
+      "#{date.month}/#{date.day}"
     end
   end
 
@@ -36,4 +38,24 @@ class TreehouseChart
     end
   end
 
+  def self.count_badges_by_day(x_axis_days_array, badges_array)
+    hash = Hash.new(0)
+
+    # If user didnt earn a badge for a day, that day's value = 0
+    x_axis_days_array.each do |day|
+      hash[day] = 0
+    end
+
+    # Each day's value = number of badges for that day
+    badges_array.each do |day|
+      hash[day] += 1
+    end
+
+    collect_badges(hash.values)
+  end
+
+  def self.collect_badges(badges_array)
+    sum = 0
+    badges_array.collect { |int| sum += int }
+  end
 end
