707a8e270b38c246824e3d79e33d3e4770821731
Victoria Friedman <victoria.m.friedman@gmail.com>
2013-04-17 11:13:02 -0400

    took logic out of views, and put into model

--- app/helpers/achievements_helper.rb
+++ app/helpers/achievements_helper.rb
@@ -1,9 +1,5 @@
 module AchievementsHelper
 
-   def self.student_message(user)
-    student_star = Star.where(:name => 'Gifted Star').first.id
-    @user.achievements.where("message IS NOT NULL AND star_id = #{student_star}")
-  end
 
 
 end
