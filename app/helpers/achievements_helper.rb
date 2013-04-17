1caa1592c6429b3bda2988bc901336f7335b9a46
Victoria Friedman <victoria.m.friedman@gmail.com>
2013-04-17 11:06:52 -0400

    fix so only student stars show up in student star

--- app/helpers/achievements_helper.rb
+++ app/helpers/achievements_helper.rb
@@ -1,2 +1,9 @@
 module AchievementsHelper
+
+   def self.student_message(user)
+    student_star = Star.where(:name => 'Gifted Star').first.id
+    @user.achievements.where("message IS NOT NULL AND star_id = #{student_star}")
+  end
+
+
 end
--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -11,6 +11,12 @@ class Achievement < ActiveRecord::Base
   #   star_id = star.id
   # end
 
+  def self.student_message(user)
+    student_star = Star.where(:name => 'Gifted Star').first.id
+    user.achievements.where("message IS NOT NULL AND star_id = #{student_star}")
+  end
+
+
   def distance_of_time_in_days_hours_and_minutes(from_time, to_time)
     from_time = from_time.to_time if from_time.respond_to?(:to_time)
     to_time = to_time.to_time if to_time.respond_to?(:to_time)
